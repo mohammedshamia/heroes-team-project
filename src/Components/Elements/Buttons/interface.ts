2f6aad593066b8cc516318b087a43d04c6932091
eman ebrahim <adla48770@gmail.com>
2022-01-14 00:34:40 +0200

    layout card

--- src/Components/Elements/Buttons/interface.ts
+++ src/Components/Elements/Buttons/interface.ts
@@ -2,10 +2,10 @@ export interface ButtonProp {
   border?: string;
   color?: string;
   children?: string | React.ReactNode;
-  height: string;
-  width: string;
+  height?: string;
+  width?: string;
   onClick?: () => void;
-  bold: boolean;
+  bold?: boolean;
   backgroundColor?: string;
   fontSize?: string;
   hover?: boolean;
--- src/Components/Elements/Rating/index.tsx
+++ src/Components/Elements/Rating/index.tsx
@@ -1,18 +1,26 @@
 import React, { useMemo, useState } from "react";
-import { Star } from './style'
+import EmptyStar from "../../../Assets/EmptyStar";
+import FilledStar from "../../../Assets/FilledStar";
+// import { Star } from './style'
 interface PropRating {
-  count: number,
+  count?: number,
   rating: number,
   color: {
     filled: string,
     unfilled: string
   }
-
+  onRating: any; 
 }
 const Rate = ({ count, rating, color, onRating }: PropRating) => {
   const [hoverRating, setHoverRating] = useState<number>(0);
 
+  const handleRating = (index: number) => {
+    onRating(index + 1)
+    // setEmpty(5 - (index + 1))
+  }
+
   const getColor = (index: any) => {
+
     if (hoverRating >= index) {
       return color.filled;
     } else if (!hoverRating && rating >= index) {
@@ -23,30 +31,32 @@ const Rate = ({ count, rating, color, onRating }: PropRating) => {
   };
 
   const starRating = useMemo(() => {
-    return Array(count)
-      .fill(0)
-      .map((_, i) => i + 1)
-      .map((idx) => (
-        <Star
-          key={idx}
-          onClick={() => onRating(idx)}
-          className="cursor-pointer"
-          style={{ color: getColor(idx) }}
-          onMouseEnter={() => setHoverRating(idx)}
-          onMouseLeave={() => setHoverRating(0)}
-
-        >&#9733;</Star>
+    return Array(count)//3
+      .fill(0).map((_, i) => i + 1)
+      .map((idx, index) => (
+        <FilledStar key={  idx} onClick={() => handleRating(index)}  color={getColor(idx) } onMouseEnter={() => setHoverRating(idx)} onMouseLeave={() => setHoverRating(0)} />
       ));
   }, [count, rating, hoverRating]);
 
-  return <div>{starRating}</div>;
+  // const endRating = useMemo(() => {
+  //   return Array(empty)//2
+  //     .fill(0)
+  //     .map((idx, index) => (
+  //       <EmptyStar key={"empty" + idx} onClick={() => handleRating(index + rating)} style={{ color: getColor(idx) }} onMouseEnter={() => setHoverRating(idx)} onMouseLeave={() => setHoverRating(0)} />
+  //     ));
+  // }, [count, rating, hoverRating]);
+
+  return <div>{starRating} </div>;
 };
 Rate.defaultProps = {
   count: 5,
+  // filled: 2,
+  // empty: 5 - filled,
   rating: 0,
   color: {
-    filled: "#f5eb3b",
-    unfilled: "#DCDCDC",
+    filled: "#FCDD06",
+    unfilled: "#140d0d",
   },
+
 };
 export default Rate;
