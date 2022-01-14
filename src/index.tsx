a51105af3499f16447683da9a2abed1df772f1b0
Ahmed .M A. Ghanim <42157141+ham1o7d@users.noreply.github.com>
2022-01-14 17:07:33 +0200

    I created error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ErrorBoundary from "./Components/Elements/ErrorBoundary/index";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById("root")
 );
