9ce71d716720f25e0dc78725b489f04bfb40ba8e
Mohammed Shoman <mohammedshoman9@gmail.com>
2022-01-16 01:02:27 +0200

    fix issue

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ErrorBoundary from "./Components/Elements/ErrorBoundary/index";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
