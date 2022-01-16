4af26133291c337a97fa1a0f777df5f2a0620c2d
Mohammed Shoman <mohammedshoman9@gmail.com>
2022-01-17 00:49:43 +0200

    create admin pages

--- src/Pages/AdminPages/ProductsAdminPage/index.tsx
+++ src/Pages/AdminPages/ProductsAdminPage/index.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import AdminTable from "../../../Components/AdminTable";
+import Container from "../../../Components/Container";
 
 const ProductsAdminPage = () => {
-  return <div>ProductsAdminPage</div>;
+  return (
+    <div>
+      <Container>
+        <AdminTable
+          data={[
+            { id: "1", name: "new", price: "$100", category: "new" },
+            { id: "2", name: "new", price: "$100", category: "new" },
+            { id: "3", name: "new", price: "$100", category: "new" },
+            { id: "4", name: "new", price: "$100", category: "new" },
+          ]}
+          headOne={"PRODUCT ID"}
+          headTwo={"PRODUCT NAME"}
+          headThree={"PRODUCT PRICE"}
+          headFour={"CATEGORY"}
+          headFive={"ACTION"}
+          tableName={"Products"}
+          handleDeleteRow={() => {}}
+          handleEditRow={() => {}}
+          handleCreateRow={() => {}}
+        />
+      </Container>
+    </div>
+  );
 };
 
 export default ProductsAdminPage;
