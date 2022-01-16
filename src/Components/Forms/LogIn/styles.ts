28cba7abdb3048e099202563dcdd4ae2de1f6cba
Ahmed .M A. Ghanim <42157141+ham1o7d@users.noreply.github.com>
2022-01-16 20:42:30 +0200

    I created Signup page

--- src/Components/Forms/LogIn/styles.ts
+++ src/Components/Forms/LogIn/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components"
 
 export const FormContainer= styled.div`
-  padding: 20px 20px 40px;
+  padding: 50px;
   border-bottom: 1px solid gray;
   width: 100%;
 `
---
+++ src/Pages/AuthPages/SignUpPage/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const SignupContainer= styled.div`
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 30% 65%;
+  overflow: hidden;
+  max-height: 100vh ;
+`
+
+export const SignupImage= styled.img`
+  /* width: 100%; */
+  height: 90%;
+  /* object-fit: contain; */
+
+`
