5b7126a492a7a3d44e965906b8a0a68b708df498
Mohammed Almallahi <almallahi@outlook.com>
2022-01-16 18:20:18 +0200

    Search

---
+++ src/Components/Elements/Search/search.styled.ts
@@ -0,0 +1,77 @@
+import styled, { css } from "styled-components";
+
+//Root Component
+export const Root = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+//Input Component
+export const ValueWrapper = styled.input`
+  width: 100%;
+  padding-left: 8px;
+  padding-right: 32px;
+  height: 32px;
+
+  border-radius: 1px;
+  border: 1px solid #b6c1ce;
+  line-height: 32px;
+`;
+
+export const Input = styled(ValueWrapper)`
+  transition: border-color 150ms linear;
+
+  &:focus {
+    border-color: #0063cc;
+    outline: none;
+  }
+`;
+
+export const AutoCompleteContainer = styled.ul`
+  background: #fff;
+  padding: 8px 0;
+  margin: 0;
+
+  list-style-type: none;
+
+  min-width: 320px;
+
+  position: absolute;
+  top: 100%;
+  left: 0;
+
+  border: 1px solid #b6c1ce;
+  border-radius: 2px;
+
+  max-height: 280px;
+
+  overflow-y: auto;
+
+  z-index: 1;
+`;
+
+export const AutoCompleteItem = styled.li`
+  padding: 0 24px;
+  width: 100%;
+
+  &:hover {
+    background-color: #ebf4ff;
+  }
+`;
+
+export const baseButtonMixin = css`
+  background: none;
+  border: none;
+  padding: 0;
+`;
+
+export const AutoCompleteItemButton = styled.button`
+  ${baseButtonMixin}
+  width: 100%;
+  line-height: 32px;
+  text-align: left;
+  &:active {
+    outline: none;
+    color: #0076f5;
+  }
+`;
