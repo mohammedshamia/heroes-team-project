98a3e1b7dac42ad5bae6be4808be6e16b521bd32
Mohammed Shoman <mohammedshoman9@gmail.com>
2022-01-16 18:13:30 +0200

    Update StepProgress.tsx

--- src/Components/Stepper/StepProgress.tsx
+++ src/Components/Stepper/StepProgress.tsx
@@ -1,5 +1,8 @@
-
-import { StepperProgress, StepperProgressWrapper, StepperProgressBar} from './style'
+import {
+  StepperProgress,
+  StepperProgressWrapper,
+  StepperProgressBar,
+} from "./style";
 interface StepperProgressProps {
   stepTitles: string[];
   currentStep: number;
@@ -13,11 +16,9 @@ export const StepProgress: React.FC<StepperProgressProps> = ({
   return (
     <StepperProgress>
       <StepperProgressWrapper>
-        <StepperProgressBar
-          style={{ width: progress + "%" }}
-        />
+        <StepperProgressBar style={{ width: progress + "%" }} />
         {stepTitles.map((title, i) => (
-          <div className="step-title" key = {i}>
+          <div className="step-title" key={i}>
             <div className="step-title-number">{i + 1}</div>
             {title}
           </div>
