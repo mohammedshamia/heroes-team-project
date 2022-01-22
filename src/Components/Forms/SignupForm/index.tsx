6804d95626f4a1033af144aa08a54f148ca85c1d
Ahmed .M A. Ghanim <42157141+ham1o7d@users.noreply.github.com>
2022-01-22 18:38:27 +0200

    add redux to signup page

--- src/Components/Forms/SignupForm/index.tsx
+++ src/Components/Forms/SignupForm/index.tsx
@@ -1,5 +1,5 @@
 import { Formik } from "formik";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { useTheme } from "styled-components";
 import { ISignUpUser } from "../../../@Types/Validation";
 import SignupSchema from "../../../Helpers/Validation/SignupSchema";
@@ -7,15 +7,28 @@ import Typography from "../../Typography";
 import { FormContainer } from "../LogInForm/styles";
 // import SignupForm from "./SignupForm";
 import SignupForm from "./SignupForm";
+import { registerUser } from "../../../Store/Slices/user";
+import { RootState, useAppDispatch } from "../../../Store/configureStore";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Signup = () => {
   const theme = useTheme();
+  const dispatch= useAppDispatch();
+  let user = useSelector((state: RootState) => state.entities.user);
+  let navigate= useNavigate();
+
+
   const LoginInitialValues: ISignUpUser = {
-    userName: "",
-    userEmail: "",
-    password: "",
-    confirmPassword: "",
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
   };
+  useEffect(()=>{
+    console.log('user', user)
+  })
   return (
     <FormContainer>
       <Typography fontWeight={700} variant="h2">
@@ -27,8 +40,12 @@ const Signup = () => {
       <Formik
         initialValues={LoginInitialValues}
         validationSchema={SignupSchema}
-        onSubmit={() => console.log("Signup Form Submited :)")}
         children={SignupForm}
+        onSubmit={(values) => {
+          console.log("Signup Form Submited :)",values);
+          dispatch(registerUser(values))
+          user.auth && navigate('');
+        }}
       />
       <div style={{ textAlign: "center", marginTop: "25px" }}>
         <NavLink
