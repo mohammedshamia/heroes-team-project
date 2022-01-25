b3cdfc0cea1914ced33ab0021f46f7135636741c
Mohammed Almallahi <almallahi@outlook.com>
2022-01-25 18:19:04 +0200

    added Types

--- src/Store/Types/index.ts
+++ src/Store/Types/index.ts
@@ -45,6 +45,11 @@ export interface IProductsSliceState {
   loading: boolean;
 }
 
+interface ItemCart {
+  product: IProduct;
+  qty: number;
+  itemTotalPrice: number;
+}
 //interface for user
 export interface IUser {
   email: string;
@@ -53,16 +58,12 @@ export interface IUser {
   profileImage: string;
   dateOfBirth: string;
   isAdmin: boolean;
-  cart?: {
-    items: [
-      {
-        items: any[];
-        totalQty: number;
-        totalPrice: number;
-      }
-    ];
+  cart: {
+    items: ItemCart[];
+    totalQty: number;
+    totalPrice: number;
   };
-  token?: string;
+  token: string;
 }
 
 export interface IusersByPaginate {
