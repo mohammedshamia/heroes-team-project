d2f753a8ad463ae981f6b691676f73367d3f45fb
Ahmed .M A. Ghanim <42157141+ham1o7d@users.noreply.github.com>
2022-01-25 22:16:00 +0200

    Adding the Modal when I want remove product from the cart

--- src/Components/Elements/Card/ShoppingCart.tsx
+++ src/Components/Elements/Card/ShoppingCart.tsx
@@ -8,10 +8,18 @@ import {
   ItemTypo,
   Itemprice,
   ContentWrapper,
+  ModalProductName,
 } from "./style";
 import CloseIcon from "../../Icons/CloseIcon";
 import { deleteItemFromCart } from "../../../Store/Slices/user";
 import { useAppDispatch } from "../../../Store/configureStore";
+import Modal from "../Modal/Dialog/Modal";
+import ModalTitle from "../Modal/Dialog/ModalTitle/ModalTitle";
+import ModalAction from "../Modal/Dialog/ModalAction/ModalAction";
+import Button from "../Buttons";
+import { useTheme } from "styled-components";
+import { useState } from "react";
+import { string } from "yup/lib/locale";
 
 interface IpropsShopCart {
   data: any;
@@ -19,8 +27,21 @@ interface IpropsShopCart {
 const ShoppingCart = ({ data }: IpropsShopCart) => {
   const { items } = data;
   const dispatch = useAppDispatch();
+  const theme = useTheme();
+
+  const [modalDisplay, setModalDisplay] = useState<boolean>(false);
+  const [proName, setProName] = useState<string>("this product");
 
   const handleRemove = (id: string) => {
+    
+  };
+
+  const openModalHandler = (productName: string) => {
+    console.log(productName, "product name");
+    setProName(productName);
+    setModalDisplay(true);
+  };
+  const closeClickHandler = (id: string) => {
     dispatch(deleteItemFromCart(id));
   };
 
@@ -28,9 +49,10 @@ const ShoppingCart = ({ data }: IpropsShopCart) => {
     <>
       {items.map((elemnt: any) => (
         <ContainerShopping key={elemnt.name + elemnt.product._id}>
-          <ContainerClose onClick={() => handleRemove(elemnt.product._id)}>
+          <ContainerClose onClick={() => openModalHandler(elemnt.product.name)}>
             <CloseIcon />
           </ContainerClose>
+
           <ItemImg alignItems="center" justifyContent="center">
             <img
               src={`${elemnt?.product?.images[0]}`}
@@ -63,6 +85,31 @@ const ShoppingCart = ({ data }: IpropsShopCart) => {
               />
             </Itemprice>
           </ContentWrapper>
+          {modalDisplay && (
+            <Modal isOpen={modalDisplay} onClose={() => setModalDisplay(false)}>
+              <ModalTitle>
+                <h4>Remove <ModalProductName>{proName}</ModalProductName> from cart?</h4>
+              </ModalTitle>
+              <ModalAction>
+                <Button
+                  style={{fontWeight: 'bold'}}
+                  onClick={() => closeClickHandler(elemnt)}
+                  backgroundColor={theme.colors.error}
+                >
+                  Remove
+                </Button>
+                <Button
+                  style={{fontWeight: 'bold'}}
+                  backgroundColor={theme.colors.primary}
+                  onClick={() => {
+                    setModalDisplay(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </ModalAction>
+            </Modal>
+          )}
         </ContainerShopping>
       ))}
     </>
