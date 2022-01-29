15ce5d67386b81f96fd45dd7e5e3bea4657eb5f0
Mohammed Shoman <mohammedshoman9@gmail.com>
2022-01-29 22:13:16 +0200

    fix upload image

--- src/Components/UploadImage/style.ts
+++ src/Components/UploadImage/style.ts
@@ -14,7 +14,7 @@ export const FileInput = styled.div<IPorps>`
       ? `linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),  url(${props.bg})`
       : ""};
   width: ${(props) => props.width || "100%"};
-  height: ${(props) => props.height || "auto"};
+  height: ${(props) => props.height || "100%"};
   position: relative;
   opacity: 0.5;
   display: flex;
